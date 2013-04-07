2617d753d3c29b31db020f1c0dfae87724145dc5
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2013-04-07 12:46:44 +0530

    WCF Host Application

---
+++ XmlWcfHostApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace XmlWcfHostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Xml Wcf Host Application";
+
+            using (ServiceHost host = new ServiceHost(
+                typeof(XmlWcfServiceLibrary.XmlWcfService)))
+            {
+                host.AddServiceEndpoint(typeof(
+                    XmlWcfServiceLibrary.IXmlWcfService),
+                    new NetTcpBinding(),
+                    "net.tcp://localhost:9001/WcfEndPoint");
+
+                host.Open();
+                Console.ReadLine();           
+            }
+        }
+    }
+}

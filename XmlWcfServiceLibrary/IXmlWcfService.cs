2b62ee1530a94f4d6cf57e2144c83e62ab6f227d
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2013-04-07 12:49:43 +0530

    WCF Service

---
+++ XmlWcfServiceLibrary/IXmlWcfService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace XmlWcfServiceLibrary
+{
+    [ServiceContract(Namespace = "www.harshits.in/Harshit/DemoApp")]
+
+    //interface declaration
+    public interface IXmlWcfService
+    {
+        [OperationContract]
+        int calculate(int a, int b, char c);
+
+    }
+}
---
+++ XmlWcfServiceLibrary/XmlWcfService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlWcfServiceLibrary
+{
+    public class XmlWcfService : IXmlWcfService
+    {
+        public int calculate(int a, int b, char c)
+        {
+
+            //accepting the data and calculating result
+            //Console.WriteLine("Recieved data from " + name);
+            //Console.WriteLine(a.ToString());
+            //Console.WriteLine(b.ToString());
+            //Console.WriteLine(c.ToString());
+            int result;
+            if (c == '+')
+                result = a + b;
+            else if (c == '-')
+                result = a - b;
+            else if (c == '*')
+                result = a * b;
+            else
+                if (b == 0)
+                    //error condition
+                    result = -1;
+                else
+                    result = a / b;
+
+            return result;
+        }
+    }
+}
